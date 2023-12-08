1ee7b11c5b6f38a9b821dbafe9192a2a2ba8f59d
Tej Lehal <tejlehal@Tejs-MacBook-Pro.local>
2023-12-08 03:27:40 -0500

    Added Readme

---
+++ variable.ts
@@ -0,0 +1,7 @@
+// string, number, boolean, null undefined
+// type inference - it will infer the type for use without putting annotaion. This will only work if you add a value on a variable.
+// type inference wont work in parameters.
+
+let myName = '';
+
+myName = 'test';
