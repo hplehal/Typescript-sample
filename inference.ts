1f524d6dd004cb622d9664e037d2bd49f1553687
Tej Lehal <tejlehal@Tejs-MacBook-Pro.local>
2023-12-08 03:34:42 -0500

    Added union and inference

---
+++ inference.ts
@@ -0,0 +1,5 @@
+// type inference in TypeScrript is when a Variable will automatically give the type base on the value given to it 
+// The catch on this is that when the variable isnt declared the variable will not be infer.
+let myName = ''
+
+myName = 'test';
--- union.ts
+++ union.ts
@@ -2,6 +2,8 @@
 // type inference - it will infer the type for use without putting annotaion. This will only work if you add a value on a variable.
 // type inference wont work in parameters.
 
-let myName = '';
+// there is a type called any which tried to give you all the datatypes.
+// Union = | its when you have two or more datatypes for a variable.
+let myName: string | null = '';
 
 myName = 'test';
