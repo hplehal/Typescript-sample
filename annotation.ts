5721f7bc1afdb896396982dbfa1769129a0d6a53
Tej Lehal <tejlehal@Tejs-MacBook-Pro.local>
2023-12-08 03:10:41 -0500

    Initial Commit

---
+++ annotation.ts
@@ -0,0 +1,9 @@
+// in order to add a Typescript file you need to add and install TS
+// npm install typescript --save-dev
+
+
+function addShipping(price: number, shipping: number): number {
+    return price + shipping;
+}
+
+addShipping(51,3);
